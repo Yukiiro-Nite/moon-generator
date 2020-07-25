768b16ea28e926dddaffe2b7aa2c0127747dd85a
Michael Bryant <e.kazuma@gmail.com>
2020-07-25 11:41:54 -0400

    Added Upper caverns and materials for upper caverns and core

--- src/main/java/me/yukiironite/MoonChunkGenerator.java
+++ src/main/java/me/yukiironite/MoonChunkGenerator.java
@@ -12,9 +12,32 @@ import java.util.List;
 import java.util.Arrays;
 
 public class MoonChunkGenerator extends ChunkGenerator {
+  public static MaterialGradient upperCavernMaterial;
+  public static MaterialGradient coreMaterial;
+
+  static {
+    upperCavernMaterial = new MaterialGradient();
+    upperCavernMaterial.nodes.add(new GradientNode(Material.STONE, 0.0, 0.0, 1.0, 1.0));
+    upperCavernMaterial.nodes.add(new GradientNode(Material.BLACKSTONE, 0.1, 1.0, 1.0, 2.0));
+    upperCavernMaterial.nodes.add(new GradientNode(Material.GILDED_BLACKSTONE, 0.2, 1.0, 1.0, 0.25));
+    upperCavernMaterial.nodes.add(new GradientNode(Material.COAL_ORE, 0.0, 0.0, 0.2, 0.05));
+    upperCavernMaterial.nodes.add(new GradientNode(Material.REDSTONE_ORE, 0.0, 1.0, 1.0, 0.025));
+    upperCavernMaterial.nodes.add(new GradientNode(Material.LAPIS_ORE, 0.0, 1.0, 1.0, 0.025));
+    upperCavernMaterial.nodes.add(new GradientNode(Material.IRON_ORE, 0.0, 0.3, 1.0, 0.05));
+    upperCavernMaterial.nodes.add(new GradientNode(Material.GOLD_ORE, 0.2, 1.0, 1.0, 0.05));
+
+    coreMaterial = new MaterialGradient();
+    coreMaterial.nodes.add(new GradientNode(Material.BLACKSTONE, -1.0, -1.0, 1.0, 2.0));
+    coreMaterial.nodes.add(new GradientNode(Material.GILDED_BLACKSTONE, -0.9, -0.4, 1.0, 0.05));
+    coreMaterial.nodes.add(new GradientNode(Material.OBSIDIAN, -0.5, 0.0, 0.5, 1.0));
+    coreMaterial.nodes.add(new GradientNode(Material.MAGMA_BLOCK, -0.4, 0.0, 0.4, 1.0));
+    coreMaterial.nodes.add(new GradientNode(Material.LAVA, -0.3, 0.0, 0.3, 1.0));
+    coreMaterial.nodes.add(new GradientNode(Material.ANCIENT_DEBRIS, -0.2, 0.0, 0.2, 0.025));
+    coreMaterial.nodes.add(new GradientNode(Material.DIAMOND_ORE, -0.2, 0.0, 0.2, 0.05));
+  }
 
   public static SimplexOctaveGenerator getNoiseGenerator(World world) {
-    SimplexOctaveGenerator generator = new SimplexOctaveGenerator(new Random(world.getSeed()), 8);
+    SimplexOctaveGenerator generator = new SimplexOctaveGenerator(world, 8);
     generator.setScale(0.01D);
 
     return generator;
@@ -35,48 +58,79 @@ public class MoonChunkGenerator extends ChunkGenerator {
   public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
     ChunkData chunk = createChunkData(world);
     SimplexOctaveGenerator generator = getNoiseGenerator(world);
+    int blockX, blockZ;
 
     for (int X = 0; X < 16; X++) {
+      blockX = chunkX*16+X;
       for (int Z = 0; Z < 16; Z++) {
+        blockZ = chunkZ*16+Z;
+
+        int surfaceHeight = getHeight(generator, blockX, blockZ, 1D, 30D, 128D);
+        int surfaceBottomHeight = getHeight(generator, blockX, blockZ, 0.5D, 5D, 70D);
+        int cavernTopHeight = getHeight(generator, blockX, blockZ, 10D, 20D, 50D);
+        int cavernBottomHeight = getHeight(generator, blockX, blockZ, 2D, 40D, 20D);
+        double noise;
+        double depth;
+
         // Set biome
-        for (int y = 255; y >= 0; y--) {
+        for (int y = 255; y >= surfaceBottomHeight; y--) {
           biome.setBiome(X, y, Z, Biome.DESERT_HILLS);
         }
-
-        int surfaceHeight = getHeight(generator, chunkX*16+X, chunkZ*16+Z, 1D, 30D, 128D);
-        int surfaceBottomHeight = getHeight(generator, chunkX*16+X, chunkZ*16+Z, 0.5D, 5D, 70D);
-        int cavernTopHeight = getHeight(generator, chunkX*16+X, chunkZ*16+Z, 10D, 20D, 50D);
-        int cavernBottomHeight = getHeight(generator, chunkX*16+X, chunkZ*16+Z, 2D, 40D, 20D);
 
         // Set surface
         for (int y = surfaceHeight; y > surfaceHeight-4; y--)
           chunk.setBlock(X, y, Z, Material.GRAVEL);
-
-        for (int y = surfaceHeight-4; y > surfaceBottomHeight; y--)
-          chunk.setBlock(X, y, Z, Material.STONE);
+        
+        // Set upper caverns
+        for (int y = surfaceHeight-4; y > surfaceBottomHeight; y--) {
+          biome.setBiome(X, y, Z, Biome.SOUL_SAND_VALLEY);
+          if(y >= surfaceHeight-6) {
+            chunk.setBlock(X, y, Z, Material.STONE);
+          } else {
+            noise = generator.noise(blockX / 2.0, y, blockZ / 2.0, 0.0, 1.375, 4.0, true);
+            if(noise >= 0.0) {
+              chunk.setBlock(X, y, Z,
+                upperCavernMaterial.getMaterial(random, noise));
+            } else {
+              chunk.setBlock(X, y, Z, Material.CAVE_AIR);
+            }
+          }
+          // chunk.setBlock(X, y, Z,
+          //   upperCavernMaterial.getMaterial(random, NumberUtil.mix(0.0, noise, depth)));
+        }
 
         // Set cavern ceiling
-        for (int y = surfaceBottomHeight; y > cavernTopHeight; y--)
+        for (int y = surfaceBottomHeight; y > cavernTopHeight; y--) {
+          biome.setBiome(X, y, Z, Biome.BASALT_DELTAS);
           chunk.setBlock(X, y, Z, Material.BASALT);
+        }
 
         // Set cavern
-        for (int y = cavernTopHeight; y > cavernBottomHeight; y--)
+        for (int y = cavernTopHeight; y > cavernBottomHeight; y--) {
+          biome.setBiome(X, y, Z, Biome.BASALT_DELTAS);
           chunk.setBlock(X, y, Z, Material.CAVE_AIR);
+        }
         
         if(random.nextDouble() < 0.01D) {
           chunk.setBlock(X, cavernTopHeight, Z, Material.CRYING_OBSIDIAN);
         }
         
         // Set core
-        for (int y = cavernBottomHeight; y > 0; y--)
-          chunk.setBlock(X, y, Z, Material.BLACKSTONE);
+        for (int y = cavernBottomHeight; y > 0; y--) {
+          biome.setBiome(X, y, Z, Biome.SOUL_SAND_VALLEY);
+          depth = 1 - Math.pow((double)y / cavernBottomHeight, 4);
+          noise = generator.noise(X, y, Z, 0.0, 1.375, 4.0, true);
+
+          chunk.setBlock(X, y, Z,
+            coreMaterial.getMaterial(random, NumberUtil.mix(-1.0, noise, depth)));
+        }
         
         // Set lava
-        for (int y = 40; y > 25; y--) {
-          if(chunk.getBlockData(X, y, Z).getMaterial().isAir()) {
-            chunk.setBlock(X, y, Z, Material.LAVA);
-          }
-        }
+        // for (int y = 40; y > 25; y--) {
+        //   if(chunk.getBlockData(X, y, Z).getMaterial().isAir()) {
+        //     chunk.setBlock(X, y, Z, Material.LAVA);
+        //   }
+        // }
 
         // Set bedrock cap
         chunk.setBlock(X, 0, Z, Material.BEDROCK);
--- src/main/java/me/yukiironite/NumberUtil.java
+++ src/main/java/me/yukiironite/NumberUtil.java
@@ -8,4 +8,17 @@ public class NumberUtil {
 
     return (value - fromStart) * scale + toStart;
   }
+
+  public static double mix(double from, double to, double amount) {
+    double clamppedAmount = clamp(amount, 0, 1);
+    return from * (1-clamppedAmount) + to * clamppedAmount;
+  }
+
+  public static double clamp(double val, double from, double to) {
+    if(from > to) {
+      return clamp(val, to, from);
+    } else {
+      return Math.max(from, Math.min(to, val));
+    }
+  }
 }
